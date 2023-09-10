cb2ba0332e87cd41364d7cf7605ba2bfd374b894
Mettius <69057048+MettiusHyper@users.noreply.github.com>
2023-09-10 18:40:33 +0200

    Initial Commit

---
+++ Example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Example
+//
+//  Created by Matteo on 10/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Matteo on 10/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
